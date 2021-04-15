e254693965f88407f11886b408e7d1a7ec595e91
Romek12345 <s-okrartes@wp.pl>
2021-04-15 21:30:11 +0200

    css

--- js.js
+++ js.js
@@ -7,50 +7,30 @@ function calculate() {
   };
   let calculationResult;
 
-
   switch (fields.operator) {
     case "+": 
-      calculationResult = dodawanie(fields.value1, fields.value2);
+      calculationResult = suma(fields.value1, fields.value2);
       break;
     case "-":
-      calculationResult = fields.value1 - fields.value2;
+      calculationResult = roznica(fields.value1, fields.value2);
       break;
     case "*":
-      calculationResult = fields.value1 * fields.value2;
+      calculationResult = iloczyn(fields.value1, fields.value2);
       break;
     case "/":
-      if (fields.value2 === 0) {
-        alert("Nie można dzielić przez zero!");
-      } else if (fields.value2 != 0) {
-        calculationResult = fields.value1 / fields.value2;
-      }
+      calculationResult = iloraz(fields.value1, fields.value2)
       break;
     case "^":
-      calculationResult = Math.pow(fields.value1, fields.value2);
+      calculationResult = potega(fields.value1, fields.value2);
       break;
     case "v":
-      calculationResult = math.pow(fields.value1, 1 / fields.value2);
+      calculationResult = pierwiastek(fields.value1, fields.value2);
+      break;
     case "!":
       console.log("tujestem");
-      calculationResult = factorial(fields.value1);
-
+      calculationResult = silnia(fields.value1);
       break;
-    /*    calculationResult = 
-          function factorial(_value1) {
-          if (fields.value1 > 1)
-            calculationResult = fields.value1 * factorial(fields.value1 - 1);
-          else
-            calculationResult = 1;
-        }; */
   }
-
-  /*         if (fields.value2 = 2){
-          calculationResult = Math.sqrt(fields.value1);
-        }else if (fields.value2 != 2) {
-          calculationResult = math.pow(fields.value1, 1/fields.value2);
-        }          
-        break;
-    */
 
   if (typeof calculationResult === "number") {
     fields.result.innerText = calculationResult;
@@ -59,19 +39,41 @@ function calculate() {
 const calculateBtn = document.getElementById("calculate");
 calculateBtn.addEventListener("click", calculate);
 
-function factorial(value1) {
+function silnia(value1) {
   console.log("rekurencja", value1);
   if (value1 === 1) {
     console.log("if", value1);
-
     return 1;
   } else {
     console.log("else", value1);
-
-    return value1 * factorial(value1 - 1);
+    return value1 * silnia(value1 - 1);
   }
 }
 
-function dodawanie(value1, value2) {
+function suma(value1, value2) {
   return value1 + value2
+}
+
+function roznica(value1, value2) {
+  return value1 - value2
+}
+
+function iloczyn(value1, value2) {
+  return value1 * value2
+}
+
+function iloraz(value1, value2) { 
+  if (value2 === 0){
+    return alert("nie mozna dzielic przez 0 balwanie")
+  } else {
+    return value1/value2
+  }
+}
+
+function potega(value1, value2) {
+  return Math.pow(value1, value2)
+}
+
+function pierwiastek(value1, value2) {
+  return Math.pow(value1, 1/value2)
 }
